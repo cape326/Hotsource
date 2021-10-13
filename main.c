09eac1a8ae41ab0f7997f97de961267601028fbe
Lee gihong <81912071+dfwerqgmsdjvn@users.noreply.github.com>
2021-10-13 18:45:50 +0900

    Update main.c

--- main.c
+++ main.c
@@ -5,5 +5,6 @@ int main()
 printf("Hello, My name is Choi-go-won\n");
 printf("Hello, My name is Bok Muchang\n");
 prinff("Hello, My name is PSJ\n");
+printf("Hello, my name is Lee gihong"\n);
 return 0;
 }
