c6986a61afa5e50050139cd5ca12e4abb1e674db
bokob <85085804+bokob@users.noreply.github.com>
2021-11-23 15:09:14 +0900

    Update main.c
    start

--- main.c
+++ main.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
+#include <time.h>
 
 int main()
 {
-printf("Hello, My name is Choi-go-won\n");
-printf("Hello, My name is Bok Muchang\n");
-prinff("Hello, My name is PSJ\n");
-printf("Hello, my name is Lee gihong"\n);
-  printf("Bye Bye\n");
+
+  
+  
 return 0;
 }
