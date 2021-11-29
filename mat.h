2a80c232913c930ed6ed1588eade7959d05d21d2
parkss404 <81512557+parkss404@users.noreply.github.com>
2021-11-29 16:16:17 +0900

    Create mat.h
    작동방식과 결과값테스트(랜덤함수,파일입출력사용X)

---
+++ mat.h
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+
+char truelist[20]="꺼매졌다";
+char falselist[20]="까매졌다";
+char question[100]="손이 ___.";
+char answer[20];
+
+printf("\n\n<두번째 게임은 맞춤법게임입니다.>\n\n");
+printf("\n%s\n\n 1.%s\n 2.%s\n\n(한글입력)",question,truelist,falselist);
+scanf("%s",answer);
+
+if(strcmp(truelist,answer) == 0)
+{
+  printf("\n 맞습니다.");
+}
+
+else if(strcmp(falselist,answer) == 0)
+{
+    printf("틀렸습니다.");
+}
+
+else
+{
+    printf("다시 입력하시오.");
+}
+
+return 0;
+}
