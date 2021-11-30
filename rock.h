588a72f7878f065c2b3d56085ca7fbf485361926
bokob <85085804+bokob@users.noreply.github.com>
2021-11-30 15:14:09 +0900

    Update rock.h
    Fix Information window.

--- rock.h
+++ rock.h
@@ -59,15 +59,15 @@ void round1()
 	int num1, answer1, num6;
 	srand(time(NULL));
 	num1 = rand() % 3 + 1;
-	printf("┏━━━━━━━━━━━━━━━┓\n");
-	printf("┃     <Round 1> 가위바위보     ┃\n");
+	printf("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n");
+	printf("┃     <Round 1> 가위바위보      ┃\n");
 	printf("┃                              ┃\n");
-	printf("┃   비기거나 지면 탈락입니다.  ┃\n");
-	printf("┃                              ┃\n");
+	printf("┃   비기거나 지면 탈락입니다.    ┃\n");
+	printf("┃                             ┃\n");
 	printf("┃           1. 가위            ┃\n");
 	printf("┃           2. 바위            ┃\n");
 	printf("┃           3. 보              ┃\n");
-	printf("┗━━━━━━━━━━━━━━━┛\n");
+	printf("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n");
 	printf("Please enter key > ");
 	scanf("%d", &answer1);
 	if (num1 == 1 && answer1 == 2 || num1 == 2 && answer1 == 3 || num1 == 3 && answer1 == 1)
