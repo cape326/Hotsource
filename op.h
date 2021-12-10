0d85091776e86ebec930acb72fa98bd13bdeab26
parkss404 <81512557+parkss404@users.noreply.github.com>
2021-12-11 01:04:38 +0900

    Update op.h
    change name

--- op.h
+++ op.h
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-int kk;
-int nn;
+int asw1;
+int asw2;
 
 
 void op(){
@@ -11,23 +11,23 @@ printf("                            1.Yes\n");
 printf("                            2.No\n");
 printf("\n                 (숫자입력):");
 
-scanf("%d",&nn);
+scanf("%d",&asw1);
 
-if(nn==1){
+if(asw1==1){
     printf("\n            오징어게임에 오신 여러분을 환영합니다.\n       456분의 1의 확률을 뚫고 최후의 일인이 되십시오.\n");
     printf("\n                     1. 첫번째 게임 시작.");
     printf("\n                     2. exit");
     printf("\n             (숫자입력):");
-    scanf("%d",&kk);
-    if(kk==1){
+    scanf("%d",&asw2);
+    if(asw2==1){
        //가위바위보 고고
     }
-    else if(kk==2){
+    else if(asw2==2){
       return 0;
     }
 }
 
-else if(nn==2){
+else if(asw1==2){
     return 0;
 }
 
