dce46dceb0a100d59355ce37ac9e89a2b0e6a628
bokob <85085804+bokob@users.noreply.github.com>
2021-12-11 14:52:02 +0900

    Update mat.h
    Add system("pause");

--- mat.h
+++ mat.h
@@ -49,6 +49,7 @@ void show(node* head, int n)
 	   printf("\t\t  /　　 )\n");
 	   printf("\t\t  ( /￣∪\n");
 	   printf("\t\t탈락입니다. 당신의 등수는 %d등 입니다.\n", Mat_Rank);
+	   system("pause");
     }
 }
 
@@ -72,7 +73,5 @@ void mat()
 
     show(head, keynum);
     fclose(fp);
-
-    return 0;
 
 }
