ce82b21b8e65bb019b57ca05d8b11e46440f5162
bokob <85085804+bokob@users.noreply.github.com>
2021-12-11 13:48:46 +0900

    Update main.c
    Add rock function buffer

--- main.c
+++ main.c
@@ -11,8 +11,12 @@ int main()
 {
   op();
   while (getchar() != '\n');
+  rock();
+  while (getchar() != '\n');
   mat();
+  while (getchar() != '\n');
   new();
+  while (getchar() != '\n');
   Glass_Game();
   
   return 0;
